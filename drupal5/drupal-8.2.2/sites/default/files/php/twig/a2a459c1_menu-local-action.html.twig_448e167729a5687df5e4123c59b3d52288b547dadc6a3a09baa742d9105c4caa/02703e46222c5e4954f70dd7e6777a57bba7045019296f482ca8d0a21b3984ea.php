5e1fd00918cabc7d138db88e2ebfd79f1b3c0013
MaximeSIMET <maxime.simet@yahoo.fr>
2016-11-06 17:18:51 +0100

    drupal1

---
+++ drupal5/drupal-8.2.2/sites/default/files/php/twig/a2a459c1_menu-local-action.html.twig_448e167729a5687df5e4123c59b3d52288b547dadc6a3a09baa742d9105c4caa/02703e46222c5e4954f70dd7e6777a57bba7045019296f482ca8d0a21b3984ea.php
@@ -0,0 +1,79 @@
+<?php
+
+/* core/themes/classy/templates/navigation/menu-local-action.html.twig */
+class __TwigTemplate_2880d3ddb7de4d427de6bab34869c3e358be32c0ed5ca261a3ffe6c47bb2ddcc extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 13
+        echo "<li";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["attributes"]) ? $context["attributes"] : null), "html", null, true));
+        echo ">";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["link"]) ? $context["link"] : null), "html", null, true));
+        echo "</li>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/classy/templates/navigation/menu-local-action.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 13,);
+    }
+}
+/* {#*/
+/* /***/
+/*  * @file*/
+/*  * Theme override for a single local action link.*/
+/*  **/
+/*  * Available variables:*/
+/*  * - attributes: HTML attributes for the wrapper element.*/
+/*  * - link: A rendered link element.*/
+/*  **/
+/*  * @see template_preprocess_menu_local_action()*/
+/*  *//* */
+/* #}*/
+/* <li{{ attributes }}>{{ link }}</li>*/
+/* */
